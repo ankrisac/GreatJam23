eca599398d55c4b706eae8b593f11aefe26f5b62
ankrisac <ankrisac@gmail.com>
2023-02-03 13:39:56 +0000

    setup Graphics, Surface

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::borrow::Cow::Borrowed;
+
 use winit::{
     event::{KeyboardInput, VirtualKeyCode},
     event_loop::ControlFlow,
@@ -36,12 +38,12 @@ impl Surface {
     }
 }
 
-struct Context {
+struct Graphics {
     surface: Surface,
     device: wgpu::Device,
     queue: wgpu::Queue,
 }
-impl Context {
+impl Graphics {
     async fn new(window: &winit::window::Window) -> Self {
         let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
             backends: wgpu::Backends::all(),
@@ -80,8 +82,144 @@ impl Context {
     fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
         self.surface.resize(&self.device, new_size);
     }
-    fn render(&mut self) {
+    fn get_frame(&mut self) -> Option<wgpu::SurfaceTexture> {
+        use wgpu::SurfaceError::*;
 
+        match self.surface.handle.get_current_texture() {
+            Ok(frame) => return Some(frame),
+            Err(err) => match err {
+                Timeout => {}
+                Outdated | Lost => self.surface.resize(&self.device, self.surface.size),
+                OutOfMemory => panic!("Error out of memory"),
+            },
+        }
+
+        None
+    }
+
+    fn load_shader(&self, path: &str) -> wgpu::ShaderModule {
+        let source = std::fs::read_to_string(path).expect("unable to read file");
+
+        self.device
+            .create_shader_module(wgpu::ShaderModuleDescriptor {
+                label: Some(path),
+                source: wgpu::ShaderSource::Wgsl(Borrowed(source.as_str())),
+            })
+    }
+}
+
+struct Blitz {
+    pipeline: wgpu::RenderPipeline,
+}
+impl Blitz {
+    const LABEL: Option<&str> = Some("Blitz");
+
+    fn new(gfx: &Graphics) -> Self {
+        let layout = gfx
+            .device
+            .create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                label: Self::LABEL,
+                bind_group_layouts: &[],
+                push_constant_ranges: &[],
+            });
+
+        let shader = gfx.load_shader("shaders/blitz.wgsl");
+
+        let pipeline = gfx
+            .device
+            .create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+                label: Self::LABEL,
+                layout: Some(&layout),
+                vertex: wgpu::VertexState {
+                    module: &shader,
+                    entry_point: "vert_main",
+                    buffers: &[],
+                },
+                primitive: wgpu::PrimitiveState {
+                    topology: wgpu::PrimitiveTopology::TriangleList,
+                    strip_index_format: None,
+                    front_face: wgpu::FrontFace::Ccw,
+                    cull_mode: None,
+                    unclipped_depth: false,
+                    polygon_mode: wgpu::PolygonMode::Fill,
+                    conservative: false,
+                },
+                depth_stencil: None,
+                multisample: wgpu::MultisampleState {
+                    count: 1,
+                    mask: !0,
+                    alpha_to_coverage_enabled: false,
+                },
+                fragment: Some(wgpu::FragmentState {
+                    module: &shader,
+                    entry_point: "frag_main",
+                    targets: &[Some(wgpu::ColorTargetState {
+                        format: wgpu::TextureFormat::Bgra8UnormSrgb,
+                        blend: Some(wgpu::BlendState::REPLACE),
+                        write_mask: wgpu::ColorWrites::ALL,
+                    })],
+                }),
+                multiview: None,
+            });
+
+        Self { pipeline }
+    }
+
+    fn render<'a>(&'a self, pass: &mut wgpu::RenderPass<'a>) {
+        pass.set_pipeline(&self.pipeline);
+        pass.draw(0..3, 0..1);
+    }
+}
+
+struct State {
+    gfx: Graphics,
+    blitz: Blitz,
+}
+impl State {
+    fn new(gfx: Graphics) -> Self {
+        let blitz = Blitz::new(&gfx);
+
+        Self { gfx, blitz }
+    }
+    fn render(&mut self) -> Option<()> {
+        let frame = self.gfx.get_frame()?;
+
+        let view = frame
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor::default());
+
+        let mut encoder = self
+            .gfx
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("state"),
+            });
+        {
+            let mut pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("state"),
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.0,
+                            g: 1.0,
+                            b: 0.0,
+                            a: 0.0,
+                        }),
+                        store: true,
+                    },
+                })],
+                depth_stencil_attachment: None,
+            });
+
+            self.blitz.render(&mut pass);
+        }
+        self.gfx.queue.submit(std::iter::once(encoder.finish()));
+
+        frame.present();
+
+        Some(())
     }
 }
 
@@ -93,7 +231,8 @@ fn main() {
         .build(&event_loop)
         .expect("Failed to create window");
 
-    let mut context = pollster::block_on(Context::new(&window));
+    let context = pollster::block_on(Graphics::new(&window));
+    let mut state = State::new(context);
 
     event_loop.run(move |event, _, flow| match event {
         winit::event::Event::WindowEvent { window_id, event } => {
@@ -114,15 +253,17 @@ fn main() {
                     } => {
                         *flow = ControlFlow::Exit;
                     }
-                    WindowEvent::Resized(new_size) => context.resize(new_size),
+                    WindowEvent::Resized(new_size) => state.gfx.resize(new_size),
                     WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
-                        context.resize(*new_inner_size)
+                        state.gfx.resize(*new_inner_size)
                     }
                     _ => {}
                 }
             }
         }
-        winit::event::Event::MainEventsCleared => {}
+        winit::event::Event::MainEventsCleared => {
+            state.render();
+        }
         _ => {}
     });
 }
